292b9eea938a81304b405af4fcd704c4e8da9300
jeffotoni <jeff.otoni@gmail.com>
2023-03-01 15:23:03 -0300

    correcao codigo basicauth

--- middleware/basicauth/basicauth.go
+++ middleware/basicauth/basicauth.go
@@ -1,43 +1,51 @@
 package main
 
-import "github.com/gojeffotoni/quick"
-       "net/http"
+import (
+	"log"
+	"net/http"
 
+	"github.com/gojeffotoni/quick"
+)
+
+// curl -i -H "Block:true" -XGET localhost:8080/v1/blocked
 func main() {
-app.Use(func(h http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-      //This is the third middleware! This guy blocks request if header contains Block == true
-      if r.Header.Get("Block") == "" {
-        w.WriteHeader(400)
-        w.Write([]byte("Message": "send the block header, please! :("))
-        return
-      }
 
-      if r.Header.Get("Block") == "true" {
-        w.WriteHeader(200)
-        w.Write([]byte("Message": "quicks block this request :)"))
-        return
-      }
-      h.ServeHTTP(w, r)
-    })
-  })
+	app := quick.New()
 
-  app.Get("/v1/blocked", func(c *quick.Ctx) {
-    c.Set("Content-Type", "application/json")
+	app.Use(func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//Este middleware, irá bloquear sua requisicao se não passar header Block:true
+			if r.Header.Get("Block") == "" || r.Header.Get("Block") == "false" {
+				w.WriteHeader(400)
+				w.Write([]byte(`{"Message": "Envia block em seu header, por favor! :("}`))
+				return
+			}
 
-    type my struct {
-      Msg   string json:"msg"
-      Block string json:"block_message"
-    }
+			if r.Header.Get("Block") == "true" {
+				w.WriteHeader(200)
+				w.Write([]byte(""))
+				return
+			}
+			h.ServeHTTP(w, r)
+		})
+	})
 
-    log.Println(c.Headers["Messageid"])
+	app.Get("/v1/blocked", func(c *quick.Ctx) {
+		c.Set("Content-Type", "application/json")
 
-    c.Status(200).JSON(&my{
-      Msg:   "Quick ❤️",
-      Block: c.Headers["Block"][0],
-    })
-  })
+		type my struct {
+			Msg   string `json:"msg"`
+			Block string `json:"block_message"`
+		}
 
-  log.Fatal(app.Listen("0.0.0.0:8080"))
+		log.Println(c.Headers["Messageid"])
+
+		c.Status(200).JSON(&my{
+			Msg:   "Quick ❤️",
+			Block: c.Headers["Block"][0],
+		})
+	})
+
+	log.Fatal(app.Listen("0.0.0.0:8080"))
 
 }

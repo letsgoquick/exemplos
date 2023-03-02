49297e666387ea9a65607f6fbae27a78a34db5b1
jeffotoni <jeff.otoni@gmail.com>
2023-03-02 03:44:17 -0300

    regex

---
+++ quick.regex/quick.regex.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gojeffotoni/quick"
+	"github.com/gojeffotoni/quick/middleware/msgid"
+)
+
+func main() {
+	app := quick.New()
+
+	app.Use(msgid.New())
+
+	app.Get("/v1/user/{[0-9]}", func(c *quick.Ctx) {
+		c.Set("Content-Type", "application/json")
+		c.Status(200).String("Quick ação total!!!")
+		return
+	})
+
+	app.Listen("0.0.0.0:8080")
+}

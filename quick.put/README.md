d2aa10fa17b597127f9f4b6bd8462dbe1785bac6
João Eduardo <99341377+Crow3442@users.noreply.github.com>
2023-03-02 16:46:19 -0300

    README

---
+++ quick.put/README.md
@@ -0,0 +1,26 @@
+# Put
+
+O método Put() no Framework Quick é usado para definir uma rota HTTP que responde a solicitações PUT. O método Put() é um método HTTP que permite que os clientes enviem dados para um servidor para atualização de recursos. Em termos simples, ele permite que um cliente envie uma atualização para um recurso existente em um servidor.
+
+Para definir uma rota PUT usando o Quick, basta chamar o método Put() em uma instância de aplicativo Quick. O método Put() aceita dois argumentos: o primeiro é o caminho da rota, e o segundo é o manipulador de rota, que é uma função que é chamada sempre que a rota é correspondida.
+
+```go
+package main
+
+import (
+	"github.com/gojeffotoni/quick"
+)
+
+func main() {
+	app := quick.New()
+
+	app.Put("/users/:id", func(c *quick.Ctx) {
+		userID := c.Param("id")
+		// Lógica de atualização do usuário
+		c.SendString("Usuário " + userID + " atualizado com sucesso!")
+	})
+
+	app.Listen(":8080")
+}
+```
+
--- quick.put/put.go
+++ quick.put/put.go
@@ -1,51 +1,17 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gojeffotoni/quick"
 )
 
-// curl -i -H "Block:true" -XGET localhost:8080/v1/blocked
 func main() {
-
 	app := quick.New()
 
-	app.Use(func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//Este middleware, irá bloquear sua requisicao se não passar header Block:true
-			if r.Header.Get("Block") == "" || r.Header.Get("Block") == "false" {
-				w.WriteHeader(400)
-				w.Write([]byte(`{"Message": "Envia block em seu header, por favor! :("}`))
-				return
-			}
-
-			if r.Header.Get("Block") == "true" {
-				w.WriteHeader(200)
-				w.Write([]byte(""))
-				return
-			}
-			h.ServeHTTP(w, r)
-		})
+	app.Put("/users/:id", func(c *quick.Ctx) {
+		userID := c.Param("id")
+		// Lógica de atualização do usuário
+		c.SendString("Usuário " + userID + " atualizado com sucesso!")
 	})
 
-	app.Get("/v1/blocked", func(c *quick.Ctx) {
-		c.Set("Content-Type", "application/json")
-
-		type my struct {
-			Msg   string `json:"msg"`
-			Block string `json:"block_message"`
-		}
-
-		log.Println(c.Headers["Messageid"])
-
-		c.Status(200).JSON(&my{
-			Msg:   "Quick ❤️",
-			Block: c.Headers["Block"][0],
-		})
-	})
-
-	log.Fatal(app.Listen("0.0.0.0:8080"))
-
+	app.Listen(":8080")
 }
